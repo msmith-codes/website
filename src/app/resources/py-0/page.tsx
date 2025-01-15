7179551c982715565fea475d669b87330e46f394
Michael Smith <msmith.softwaredev@gmail.com>
2025-01-15 17:18:42 -0500

    Added page

---
+++ src/app/resources/py-0/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Python0Page() : React.ReactElement
+{
+    return (
+        <div>
+            <main>
+                <h1>/* Setting up Python */</h1>
+                <p>
+                    Under construction.
+                </p>
+            </main>
+        </div>
+    );
+}
