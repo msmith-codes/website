7696be314690836f0aa107774a085126ab355878
msmith-codes <msmith.softwaredev@gmail.com>
2025-03-06 14:59:44 -0500

    Readded Resources Tab

--- src/app/_components/Navbar.tsx
+++ src/app/_components/Navbar.tsx
@@ -13,7 +13,7 @@ export default function NavbarComponent()
         <div id="navbar">
             <Link id={currentPath === "/" ? "navitem-active" : "navitem"} href="/">Home</Link> •
             <Link id={currentPath.startsWith("/blog") ? "navitem-active" : "navitem"} href="/blog">Blog</Link> •
-            { /*           <Link id={currentPath.startsWith("/resources") ? "navitem-active" : "navitem"} href="/resources">Resources</Link> • */ }
+            <Link id={currentPath.startsWith("/resources") ? "navitem-active" : "navitem"} href="/resources">Resources</Link> •
             <Link id={currentPath === "/experience" ? "navitem-active" : "navitem"} href="/experience">Experience</Link> •
             <Link id={currentPath === "/projects" ? "navitem-active" : "navitem"} href="/projects">Projects</Link> •
             <Link id={currentPath === "/contact" ? "navitem-active" : "navitem"} href="/contact">Contact</Link> 
